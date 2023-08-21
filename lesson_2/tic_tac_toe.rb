0585abd9505b525f7df3100a8adadd560ce86cbd
Jasmynn Whitfield <jwhitfield.dev@gmail.com>
2023-08-21 19:53:17 -0400

    Add logic to display the state of each square in the board

--- lesson_2/tic_tac_toe.rb
+++ lesson_2/tic_tac_toe.rb
@@ -18,18 +18,26 @@ Player
 =end
 
 class Board
+  INITIAL_MARKER = " "
+
   def initialize
-    #need some way to model the 3x3 grid. Maybe "sqaures"
-    #what data structure should we use?
-    # - array/hash of square objects?
-    # - array/hash of strings or integers?
+    @squares = {}
+    (1..9).each {|key| @squares[key] = Square.new(INITIAL_MARKER) }
   end 
+
+  def get_square_at(key)
+    @squares[key]
+  end
 end 
 
 class Square 
-  def initialize
-    #maybe a "status" to keep track of this sqaure's mark
+  def initialize(marker)
+    @marker = marker
   end
+
+  def to_s 
+    @marker
+  end 
 end 
 
 class Player 
@@ -42,17 +50,49 @@ class Player
 end 
 
 class TTTGame 
+  attr_reader :board
+
+  def initialize 
+    @board = Board.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
+    puts "     |     |"
+    puts ""
+  end
+
   def play 
     display_welcome_message
     loop do 
       display_board
+      break
       first_player_moves 
       break if someone_won? || board_full?
 
       second_player_moves
       break if someone_won? || board_full?
     end
-    display_result 
+    # display_result 
     display_goodbye_message
   end
 end
