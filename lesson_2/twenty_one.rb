4baf14ffca758247e7cba3993ecc24a4821648e2
Jasmynn Whitfield <jwhitfield.dev@gmail.com>
2023-08-26 13:39:20 -0400

    Add logic to check the total of each players cards

--- lesson_2/twenty_one.rb
+++ lesson_2/twenty_one.rb
@@ -1,58 +1,3 @@
-=begin
-Twenty-One is a card game consisting of a dealer and a player, where the participants try to get as close to 21 as possible without going over.
-
-Here is an overview of the game:
--Both participants are initially dealt 2 cards from a 52-card deck.
--The player takes the first turn and can 'hit' or 'stay'.
--If the player busts, he loses. If he stays, it's the dealer's turn.
--The dealer must hit until his cards add up to at least 17.
--If he busts, the player wins. If both player and dealer stay, then the highest total wins.
--If both totals are equal, then it's a tie, and nobody wins.
-
-Nouns: card, player, dealer, participant, deck, game, total
-Verbs: deal, hit, stay, busts
-
-Player 
--hit
--stay
--busted?
--total
-
-Dealer
--hit
--stay
--busted?
--total
--deal?
-
-Participant
-
-Deck 
--deal?
-
-Card
-
-Game
--start
-=end
-
-# module Hand
-#   def initialize
-#   end
-
-#   def hit 
-#   end
-
-#   def stay 
-#   end 
-
-#   def busted?
-#   end
-
-#   def total 
-#   end
-# end
-
 # class Player < Participant
 #   include Hand
 # end 
@@ -147,6 +92,37 @@ class Deck
   end
 end 
 
+module Hand
+  def show_hand
+    puts "----#{name}'s Hand ----"
+    cards.each do |card|
+      puts "=> #{card}"
+    end
+    puts "=> Total: #{total}"
+    puts ""
+  end
+
+  def total 
+    total = 0
+    cards.each do |card|
+      if card.ace?
+        total += 11
+      elsif card.jack? || card.queen? || card.king?
+        total += 10 
+      else 
+        total += card.face.to_i
+      end
+    end
+
+    # correct for Aces 
+    cards.select(&:ace?).count.times do 
+      break if total <= 21
+      total -= 10 
+    end
+
+    total
+  end
+end
 
 class Game 
   def start 
